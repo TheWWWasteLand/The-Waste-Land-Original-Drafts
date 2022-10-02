255448bfb8f67db76db60ea80cd852db12321cec
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-02 20:25:31 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -19,7 +19,7 @@ window.onload = function() {
       }
       else if (strLength > 11) {
         var newLength = 0.35 * strLength;
-        var padd = '0.5em';
+        var padd = '0.6em';
       }
       var finalLength = 0 - newLength;
       var dist = finalLength + 'em';
