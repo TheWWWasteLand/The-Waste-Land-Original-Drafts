f3e82d13ae3131fa2348bd0784c6c3f2a6d587dc
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-02 18:55:07 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -3,8 +3,8 @@ window.onload = function() {
   const note = document.getElementsByTagName("tei-note");
   for (let i = 0; i < add.length; i++) {
     add[i].classList.add('hidden');
-    var strLength = 1.5 * (0 - add[i].innerText.length);
-    var dist = strLength + 'en';
+    var strLength = 0.5 * (0 - add[i].innerText.length);
+    var dist = strLength + 'em';
     console.log(dist);
     add[i].style.marginLeft = dist;
     console.log(add[i])
