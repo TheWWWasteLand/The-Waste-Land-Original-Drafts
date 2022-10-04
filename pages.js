fc92e4ed0b1e9b99e0ad63c773132fad97887448
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-04 11:18:14 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -250,12 +250,14 @@ function finalVer() {
         if (adds[i].getAttribute('rend').includes('linking-line')) {
           console.log(adds[i]);
           var chs = parseInt(adds[i].getAttribute('rend'));
+          console.log(chs)
           if (adds[i].getAttribute('rend').includes('diagonal')) {
             var chs = chs / 0.82;
           }
+          var prevSpace = (chs * 0.42) + 'em';
+          console.log(prevSpace);
+          adds[i].setAttribute('data-before', prevSpace);
         }
-        var prevSpace = (chs * 0.42) + 'em';
-        adds[i].setAttribute('data-before', prevSpace);
       }
     }
     for (let i = 5; i < 18; i++) {
