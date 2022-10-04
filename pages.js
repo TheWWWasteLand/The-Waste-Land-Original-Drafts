2ffe72f96a94f5240b303a5f025bb51777145945
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-04 17:19:00 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -255,7 +255,7 @@ function finalVer() {
           for (let i = 0; i < (1.5 * chs); i++) {
             prevSpace = prevSpace + " ";
           }
-          prevSpace = prevSpace + adds[i].innerText;
+          prevSpace = adds[i].innerText + prevSpace;
           console.log(prevSpace);
           adds[i].setAttribute('data-before', prevSpace);
         }
