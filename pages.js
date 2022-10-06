f29a1ef6ca29769f00e78e1de0562ecbdf138bb0
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-06 23:25:20 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -260,7 +260,7 @@ function finalVer() {
           for (let i = 0; i < (1.5 * chs); i++) {
             prevSpace = prevSpace + " ";
           }
-          var marginleft = (0 - (0.3 * chs) - (0.42 * textLength )) + 'em';
+          var marginleft = (0 - ((textLength + chs) * 0.42)) + 'em';
           adds[i].setAttribute('data-before', prevSpace);
           adds[i].style.marginLeft = marginleft;
         }
