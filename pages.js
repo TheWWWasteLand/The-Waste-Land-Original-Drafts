e400f908905a742ddc7810cdbaac6f3f31c05c91
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-07 14:44:59 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -28,7 +28,7 @@ window.onload = function() {
     }
     else if (add[i].hasAttribute('rend')) {
       console.log(add[i]);
-      if (add[i].getAttribute('place') == 'margin-left') {
+      if (add[i].getAttribute('place').includes('margin-left')) {
         var strLength = add[i].innerText.length;
         var regex = /^\d+$/;
         var chs = parseInt(add[i].getAttribute('rend').match(regex));
