f04ede97c3f19a0ef21810cc3820d0c7e253b590
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-21 11:27:07 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -435,7 +435,7 @@ $(document).ready(function(){
   $("#textOnly").click(function(){
     $("tei-figure").hide();
     $(".zoom-button").hide();
-    $("[type=poemPage]").css({"margin-left": "12vw"});
+    $("[type=poemPage]").css({"margin-left": "0px", "width": "calc(100% - 126px)"});
     $("#button-notes-list").css({"left": "65.5px", "width": "calc(100vw - 124.5px)"});
   });
 });
