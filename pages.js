128524c19a88c62efd2050475cca234af918d868
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-10-21 09:58:07 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -4,7 +4,7 @@ window.onload = function() {
   const graphs = document.getElementsByTagName("tei-graphic");
   for (let g = 0; g < graphs.length; g++) {
     if (graphs[g].parentNode.nodeName != "TEI-FIGURE") {
-      document.appendChild(graphs[g]);
+      document.body.appendChild(graphs[g]);
       graphs[g].classList.add('hidden');
     }
   }
