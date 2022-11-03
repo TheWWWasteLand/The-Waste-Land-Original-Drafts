337c59645b0f9914d06c1f5098f66b125272a540
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-11-03 14:55:16 +0100

    Update pages.js

--- pages.js
+++ pages.js
@@ -473,15 +473,13 @@ function TEliot() {
 function singlePage() {
     $("tei-figure").hide();
     $(".zoom-button").hide();
-    $("[type=poemPage]").css({"margin-left": "0px", "width": "calc(100% - 126px)", "padding-left": "300px"});
+    $("[type=poemPage]").css({"margin-left": "0px", "width": "calc(100% - 126px)", "padding-left": "300px", "height": "auto", "overflow":"auto"});
     $("#button-notes-list").css({"left": "65.5px", "width": "calc(100vw - 124.5px)"});
     var a = 0;
     $("tei-div").each(function(){
-        a = a + 700;
-        console.log(a);
+        a = a + 600;
         var b = (a).toString() + "px" ;
-        console.log(b);
-        $(this).css({"display": "block", "height": "100%", "margin-top" : b}); 
+        $(this).css({"display": "block", "height": "100%", "margin-top" : b});
     });
 }
 
