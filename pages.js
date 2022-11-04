d73acd39c4c0a1a9a893828429a05e884bcce22f
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-11-04 19:05:49 +0100

    Update pages.js

--- pages.js
+++ pages.js
@@ -606,8 +606,10 @@ function thumbSearch(el) {
 }
 
 function goToPage(num) {
+    console.log(num);
     const pageList = document.getElementsByTagName('tei-div');
     for (let i = 0; i < pageList.length; i++) {
+        console.log(i);
         if (pageList[i].style.display == "inline-block" && i != num) {
             pageList[i].style.display == "none";
         }
