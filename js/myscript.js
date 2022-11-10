716c3d4295885920aaa5725c656c6f3fd121a8cc
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-11-10 10:20:37 +0100

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("scroll", function(event) {
+        const animatedBoxes = document.getElementsByClassName("special-cards");
+        const windowOffsetTop = window.innerHeight + window.scrollY;
+
+        Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+            
+            const animatedBoxOffsetTop = animatedBox.offsetTop + 400;
+            console.log(animatedBoxOffsetTop);
+            if (windowOffsetTop >= animatedBoxOffsetTop) {
+                addClass(animatedBox, "slider");
+            }
+        });
+    });
+});
+
+function addClass(element, className) {
+    const arrayClasses = element.className.split(" ");
+    if (arrayClasses.indexOf(className) === -1) {
+        element.className += " " + className;
+    }
+}
